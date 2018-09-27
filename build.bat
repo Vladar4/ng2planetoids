56a4acff2eea60b0856e22cf054c82c7ad172559
Vladar4 <vladar4@gmail.com>
2018-09-27 18:38:26 +0300

    fix for Nim v0.19.0

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 cd src
 nim c --out:..\ng2planetoids.exe -d:release --opt:speed ng2planetoids.nim
-rmdir /s /q nimcache
 cd ..
 
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd src
 nim c --out:../ng2planetoids -d:release --opt:speed ng2planetoids.nim
-rm -rf nimcache
 cd ..
 
